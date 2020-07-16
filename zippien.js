141b4ee26bae8dd9522ea1418b2c49bc43f1e0ce
zippien <arulblood@gmail.com>
2020-07-16 15:23:36 +0700

    Update zippien.js

--- zippien.js
+++ zippien.js
@@ -129,7 +129,7 @@ j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.encode(a_to_vi);
-a_to_ve[i].href = "https://panelku.id/zippienlink.html?url=" + encryptedUrl;
+a_to_ve[i].href = "http://panelku.id/zippienlink.html?url=" + encryptedUrl;
 a_to_ve[i].rel = "nofollow";
 a_to_vb++;
 a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
